9e59df417d003d7f94ec3e66e799f4e0ba5b75ab
AntonioCruz26 <toniiluiz26@gmail.com>
2026-03-27 15:46:52 -0300

    Refactor hangman game logic and update .gitignore for consistency

--- jogo_de_forca.c
+++ jogo_de_forca.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 
 int main(){
@@ -11,10 +12,43 @@ int main(){
     //loop principal do programa
     int acertou = 0;
     int enforcou = 0;
+    char chute;
+
+    //quais as letras que ele chutu e guarda ela para mostrar depois
+    char chutes[26];
+
+    //quantas vezes ele chutou
+    int tentativas = 0;
 
     do{
 
-    }while(acertou == 0 || enforcou == 0);
+        //mostrar o tracejado do jogo da forca, com as letras na medida que for acertando
+        for(int i = 0; i < strlen(palavra_secreta); i++){
+
+            int achou = 0;
+            //esse loop faz com que o programa percorra o array "chutes" para colocar as letras nas lacunas 
+            for (int j = 0; j < tentativas; j++){
+                if(chutes[j] == palavra_secreta[i]){
+                    achou = 1;
+                    break;
+                }
+            }
+            if (achou){
+                printf("%c ", palavra_secreta[i]);
+            }
+            else {
+                printf("_ ");
+            }
+        }
+        printf("\n");
+
+        printf("Digite seu chute: ");
+        scanf(" %c", &chute);
+
+        chutes[tentativas] = chute;
+        tentativas++;
+
+    }while(!acertou && !enforcou);
 
     return 0;
 }
